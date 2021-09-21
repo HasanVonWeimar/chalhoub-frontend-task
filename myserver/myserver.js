5b7ef309b2d725105aab4fd775fa916878551430
AlHasan AlQaraghuli <alhasan_alaaeldin@yahoo.com>
2021-09-21 20:20:02 +0400

    updated path for get

--- myserver/myserver.js
+++ myserver/myserver.js
@@ -13,17 +13,17 @@ app.listen(port, () => {
 })
 
 app.get('/', (req, res) => {
-    const pathToData = path.resolve("../assets", "data.json")
-    console.log(pathToData)
+    const pathToData = path.resolve(__dirname, "../assets/data.json")
     fs.readFile(pathToData, 'utf8',  function(err, data){
         if(err){
             console.log("request error") //request failed
+            console.dir(err, {depth: Infinity})
             res.send("unexpected error") //todo: expect errors on client side
+            return
         }
 
         // Display the file content
         console.log(data);
         res.send(data)
-        return
     });
 })
